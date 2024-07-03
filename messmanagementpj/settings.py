fc4f046b0960650765af1d3fad697a101c55e502
Alishba Ramzan <122822965+alishbaaramzan@users.noreply.github.com>
2024-07-03 14:37:46 +0500

    Update settings.py

--- messmanagementpj/settings.py
+++ messmanagementpj/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "mess db",
         "USER": "postgres",
-        "PASSWORD": "1211",
+        "PASSWORD": "ADD YOUR OWN PASSWORD HERE",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
